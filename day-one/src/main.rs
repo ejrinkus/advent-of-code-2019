ac535ba7f3a5fedd4174fc9c8b693da1a2add9e5
Eric Rinkus <ejrinkus@gmail.com>
2019-12-06 00:35:52 -0800

    clean up day one solution and add unit tests

--- day-one/src/main.rs
+++ day-one/src/main.rs
@@ -1,26 +1,61 @@
-use std::env;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    assert_eq!(args.len(), 2, "Incorrect number of args");
+fn to_fuel(weight: &i32) -> i32 {
+    (weight / 3) - 2
+}
 
-    let mut module_fuel = 0;
-    let mut total_fuel = 0;
-    let f = File::open(&args[1]).expect("file not found");
+fn expand(module: &i32) -> i32 {
+    let mut total = 0;
+    let mut add = to_fuel(module);
+    while add > 0 {
+        total += add;
+        add = to_fuel(&add);
+    }
+    total
+}
+
+fn main() {
+    let f = File::open("day-one/input.txt").expect("file not found");
     let reader = BufReader::new(&f);
-    for line in reader.lines() {
-        let line = line.unwrap().to_string();
-        let val = line.parse::<i32>().unwrap();
-        let mut add_fuel = (val / 3) - 2;
-        module_fuel += add_fuel;
-        while add_fuel > 0 {
-            total_fuel += add_fuel;
-            add_fuel = (add_fuel / 3) - 2;
+    let result = reader.lines()
+                        .map(|l| l.unwrap())
+                        .map(|s| s.parse::<i32>().unwrap())
+                        .fold(0, |acc, input| acc + expand(&input));
+
+    println!("Sum of total fuel requirements: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_fuel() {
+        let cases = vec![
+            (12, 2),
+            (14, 2),
+            (1969, 654),
+            (100756, 33583),
+        ];
+
+        for (input, expected) in cases.iter() {
+            assert_eq!(to_fuel(&input), *expected);
         }
     }
-    println!("Sum of module fuel requirements: {}", module_fuel);
-    println!("Sum of total fuel requirements: {}", total_fuel);
+
+    #[test]
+    fn test_expand() {
+        let cases = vec![
+            (12, 2),
+            (14, 2),
+            (1969, 966),
+            (100756, 50346),
+        ];
+
+        for (input, expected) in cases.iter() {
+            assert_eq!(expand(&input), *expected);
+        }
+    }
 }
