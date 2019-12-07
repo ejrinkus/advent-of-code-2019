dc463e6e0bddf8094c2bbc73f57abb6a1c7b8e08
Eric Rinkus <ejrinkus@gmail.com>
2019-12-06 22:41:18 -0800

    day 4 part 2 solution

--- day-four/src/main.rs
+++ day-four/src/main.rs
@@ -8,22 +8,34 @@ fn to_digits(mut i: u32) -> Vec<u32> {
     digits
 }
 
+fn is_valid(digits: &Vec<u32>) -> bool {
+    if digits.len() <= 1 { return false; }
+
+    let mut found_doubles = false;
+    let mut val: u32 = digits[0];
+    let mut count: u32 = 1;
+    for i in 1..digits.len() {
+        if digits[i] < digits[i-1] {
+            return false;
+        }
+        if digits[i] == val {
+            count += 1;
+            continue;
+        }
+        if count == 2 {
+            found_doubles = true;
+        }
+        val = digits[i];
+        count = 1;
+    }
+    return found_doubles || count == 2;
+}
+
 fn main() {
     let mut count = 0;
     for combo in 172851..675869 {
         let digits: Vec<u32> = to_digits(combo);
-        let mut is_decreasing = false;
-        let mut found_doubles = false;
-        for i in 1..digits.len() {
-            if digits[i] < digits[i-1] {
-                is_decreasing = true;
-                break;
-            }
-            if digits[i] == digits[i-1] {
-                found_doubles = true;
-            }
-        }
-        if !is_decreasing && found_doubles { count += 1; }
+        if is_valid(&digits) { count += 1; }
     }
     println!("Found {} possible passwords", count);
 }
@@ -45,6 +57,22 @@ mod tests {
             assert_eq!(digits.len(), expected.len());
             let diff = digits.iter().zip(expected.iter()).filter(|&(a, b)| a != b).count();
             assert_eq!(diff, 0);
+        }
+    }
+
+    #[test]
+    fn test_is_valid() {
+        let cases = [
+            (vec![1,1,1,1,1,1], false),
+            (vec![2,2,3,4,5,0], false),
+            (vec![1,2,3,7,8,9], false),
+            (vec![1,1,2,2,3,3], true),
+            (vec![1,2,3,4,4,4], false),
+            (vec![1,1,1,1,2,2], true),
+        ];
+        for (input, expected) in cases.iter() {
+            let got = is_valid(input);
+            assert_eq!(got, *expected, "For input {:?}: expected {}, got {}", input, expected, got);
         }
     }
 }
