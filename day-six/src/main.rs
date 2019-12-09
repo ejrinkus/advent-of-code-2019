027d4fd7f3415559dbf1c1a5c598b9545feb4520
Eric Rinkus <ejrinkus@gmail.com>
2019-12-09 01:22:20 -0800

    day 6 part 2 solution

--- day-six/src/main.rs
+++ day-six/src/main.rs
@@ -21,12 +21,42 @@ use std::io::BufReader;
 //     }
 // }
 
-fn get_orbits(root: &str, bodies: &HashMap<String, Vec<String>>, depth: u32) -> u32 {
+// First return value is the total number of orbits (direct and indirect).
+// Second return value is the depth of the first planet orbited by both Santa (SAN) and you (YOU).
+// Third return value is the depth of the planet orbited by Santa (SAN).
+// Fourth return value is the depth of the planet orbited by you (YOU).
+fn trace_orbits(root: &str, bodies: &HashMap<String, Vec<String>>, depth: u32) -> (u32, u32, u32, u32) {
     let mut count = depth;
+    let mut common = 0;
+    let mut san = 0;
+    let mut you = 0;
     for s in bodies.get(root).unwrap() {
-        count += get_orbits(s, bodies, depth+1);
+        if s == "SAN" {
+            san = depth;
+            continue;
+        }
+        if s == "YOU" {
+            you = depth;
+            continue;
+        }
+
+        let (new_count, new_common, new_san, new_you) = trace_orbits(s, bodies, depth+1);
+        count += new_count;
+        if common == 0 {
+            common = new_common;
+        }
+        if san == 0 {
+            san = new_san;
+        }
+        if you == 0 {
+            you = new_you;
+        }
     }
-    return count;
+
+    if common == 0 && san != 0 && you != 0 {
+        common = depth;
+    }
+    return (count, common, san, you);
 }
 
 fn main() {
@@ -46,5 +76,9 @@ fn main() {
         bodies.entry(pieces[1].to_owned()).or_insert(Vec::new());
     }
 
-    println!("Total number of orbits: {}", get_orbits("COM", &bodies, 0));
+    let (count, common, san, you) = trace_orbits("COM", &bodies, 0);
+    println!("Total number of orbits: {}", count);
+    println!("Santa is {} jumps from COM, and you are {} jumps from COM.", san, you);
+    println!("Your nearest shared orbit is {} jumps from COM.", common);
+    println!("There are {} jumps between you and Santa.", (san - common) + (you - common));
 }
