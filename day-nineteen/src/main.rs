6e71e009c7a970efeb82e3c0baffb1eb516a93e2
Eric Rinkus <ejrinkus@gmail.com>
2019-12-27 02:25:46 -0500

    day 19 part 2 solved

--- day-nineteen/src/main.rs
+++ day-nineteen/src/main.rs
@@ -6,10 +6,7 @@ fn check(x: i64, y: i64, tape: Vec<i64>) -> bool {
     comp.pop_output().unwrap() != 0
 }
 
-fn part_one() {
-    let line = std::fs::read_to_string("day-nineteen/input.txt").expect("file not found");
-    let tape: Vec<i64> = intcode::to_tape(&line);
-
+fn part_one(tape: &Vec<i64>) {
     let mut count = 0;
     for y in 0..50 {
         for x in 0..50 {
@@ -19,6 +16,34 @@ fn part_one() {
     println!("Affected tiles: {}", count);
 }
 
+fn part_two(tape: &Vec<i64>) {
+    let mut bl = (0, 99);
+    let mut ur = (99, 0);
+    loop {
+        // Move our window right until the bottom left
+        // corner is in the beam.
+        while !check(bl.0, bl.1, tape.clone()) {
+            bl.0 += 1;
+            ur.0 += 1;
+        }
+        // Move our window down until the upper right
+        // corner is in the beam.
+        while !check(ur.0, ur.1, tape.clone()) {
+            bl.1 += 1;
+            ur.1 += 1;
+        }
+        // If the bottom left is still in the beam, then
+        // we've found our window.
+        if check(bl.0, bl.1, tape.clone()) {
+            break;
+        }
+    }
+    println!("Found window at ({}, {})", bl.0, ur.1);
+}
+
 fn main() {
-    part_one();
+    let line = std::fs::read_to_string("day-nineteen/input.txt").expect("file not found");
+    let tape: Vec<i64> = intcode::to_tape(&line);
+    part_one(&tape);
+    part_two(&tape);
 }
