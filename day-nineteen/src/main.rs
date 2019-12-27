db548d541de94775381e289288ea4ad6d5c7bbbd
Eric Rinkus <ejrinkus@gmail.com>
2019-12-27 02:11:16 -0500

    day 19 part 1 solved

--- day-nineteen/src/main.rs
+++ day-nineteen/src/main.rs
@@ -1,3 +1,24 @@
+fn check(x: i64, y: i64, tape: Vec<i64>) -> bool {
+    let mut comp = intcode::IntcodeComp::new(tape);
+    comp.push_input(x);
+    comp.push_input(y);
+    comp.start();
+    comp.pop_output().unwrap() != 0
+}
+
+fn part_one() {
+    let line = std::fs::read_to_string("day-nineteen/input.txt").expect("file not found");
+    let tape: Vec<i64> = intcode::to_tape(&line);
+
+    let mut count = 0;
+    for y in 0..50 {
+        for x in 0..50 {
+            if check(x, y, tape.clone()) { count += 1; }
+        }
+    }
+    println!("Affected tiles: {}", count);
+}
+
 fn main() {
-    println!("Hello, world!");
+    part_one();
 }
