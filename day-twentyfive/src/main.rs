f30c8deb24d71e02ab3797a239f377bc20a87739
Eric Rinkus <ejrinkus@gmail.com>
2020-01-06 00:37:32 -0800

    Day 25 finished!!!  Refactored the day 25 solution to include two function: one with a script to auto-win the game, and one to allow it to be played manually.

--- day-twentyfive/src/main.rs
+++ day-twentyfive/src/main.rs
@@ -1,14 +1,53 @@
 #[macro_use] extern crate text_io;
 
-fn main() {
-    let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("input.txt");
-    let line = std::fs::read_to_string(&path).expect("file not found");
-
-    let tape: Vec<i64> = intcode::to_tape(&line);
+fn auto_win(tape: Vec<i64>) {
     let mut comp = intcode::IntcodeComp::new(tape);
+    let quickstart = "east
+east
+take semiconductor
+north
+north
+take antenna
+south
+west
+take food ration
+west
+west
+take monolith
+east
+east
+east
+south
+east
+south
+south
+east
+east
+";
+
     comp.start();
 
-    loop {
+    for line in quickstart.lines() {
+        while let Some(_) = comp.pop_output() {
+            continue;
+        }
+
+        for c in line.as_bytes() {
+            comp.push_input(*c as i64);
+        }
+        comp.push_input(10);
+        comp.start();
+    }
+
+    while let Some(output) = comp.pop_output() {
+        print!("{}", output as u8 as char);
+    }
+}
+
+fn just_play(tape: Vec<i64>) {
+    let mut comp = intcode::IntcodeComp::new(tape);
+    comp.start();
+    while *comp.state() != intcode::IntcodeState::Finished {
         while let Some(output) = comp.pop_output() {
             print!("{}", output as u8 as char);
         }
@@ -22,4 +61,16 @@ fn main() {
 
         comp.start();
     }
+    while let Some(output) = comp.pop_output() {
+        print!("{}", output as u8 as char);
+    }
+}
+
+fn main() {
+    let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("input.txt");
+    let line = std::fs::read_to_string(&path).expect("file not found");
+
+    let tape: Vec<i64> = intcode::to_tape(&line);
+    auto_win(tape.clone());
+    just_play(tape.clone());
 }
