96fafaabc48625abd3571a9b7eeb77e86f1130b7
Eric Rinkus <ejrinkus@gmail.com>
2020-01-05 23:19:17 -0800

    Day 25.  Apparently it's a text adventure game.  No, I'm not gonna write more code to play it for me, I wanna play it myself dammit!

--- day-twentyfive/src/main.rs
+++ day-twentyfive/src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use] extern crate text_io;
+
 fn main() {
-    println!("Hello, world!");
+    let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("input.txt");
+    let line = std::fs::read_to_string(&path).expect("file not found");
+
+    let tape: Vec<i64> = intcode::to_tape(&line);
+    let mut comp = intcode::IntcodeComp::new(tape);
+    comp.start();
+
+    loop {
+        while let Some(output) = comp.pop_output() {
+            print!("{}", output as u8 as char);
+        }
+
+        let line: String = read!("{}\r\n");
+
+        for c in line.as_bytes() {
+            comp.push_input(*c as i64);
+        }
+        comp.push_input(10);
+
+        comp.start();
+    }
 }
